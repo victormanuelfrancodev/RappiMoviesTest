3f0027e6b8c187417bc73681dfc3f51505ae1d1b
victormanuelfrancodev <victormanuelfrancodev@gmail.com>
2019-05-26 23:18:59 +0900

    se remueve la extension string

--- MovieRappiTest/Helper/String+Extensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  String+Extensions.swift
-//  MovieRappiTest
-//
-//  Created by Victor Manuel Lagunas Franco on 5/21/19.
-//  Copyright © 2019 Victor Manuel Lagunas Franco. All rights reserved.
-//
-
-import Foundation
